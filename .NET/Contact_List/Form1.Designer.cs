0dee67eb91e0846b9b66888a640ed3eebbe576c6
xrzy1985 <jamespatterson1985@yahoo.com>
2018-10-08 01:59:09 -0400

    Adding to C#
    Adding a Contacts List .NET entity framework windows form application

---
+++ .NET/Contact_List/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Contacts
+{
+    partial class frmContactList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.lblLast = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblAppointment = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.comboCategory = new System.Windows.Forms.ComboBox();
+            this.txtAppointment = new System.Windows.Forms.TextBox();
+            this.First = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Last = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PhoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Appointment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnNew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.AutoSize = true;
+            this.lblFirst.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirst.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblFirst.Location = new System.Drawing.Point(50, 9);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(113, 23);
+            this.lblFirst.TabIndex = 0;
+            this.lblFirst.Text = "First Name: ";
+            this.lblFirst.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLast
+            // 
+            this.lblLast.AutoSize = true;
+            this.lblLast.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLast.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblLast.Location = new System.Drawing.Point(52, 32);
+            this.lblLast.Name = "lblLast";
+            this.lblLast.Size = new System.Drawing.Size(111, 23);
+            this.lblLast.TabIndex = 1;
+            this.lblLast.Text = "Last Name: ";
+            this.lblLast.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblEmail.Location = new System.Drawing.Point(95, 55);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(68, 23);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email: ";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPhone.Location = new System.Drawing.Point(14, 78);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(149, 23);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Phone Number: ";
+            this.lblPhone.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategory.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCategory.Location = new System.Drawing.Point(66, 101);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(97, 23);
+            this.lblCategory.TabIndex = 4;
+            this.lblCategory.Text = "Category: ";
+            this.lblCategory.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblAppointment
+            // 
+            this.lblAppointment.AutoSize = true;
+            this.lblAppointment.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppointment.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblAppointment.Location = new System.Drawing.Point(33, 124);
+            this.lblAppointment.Name = "lblAppointment";
+            this.lblAppointment.Size = new System.Drawing.Size(130, 23);
+            this.lblAppointment.TabIndex = 5;
+            this.lblAppointment.Text = "Appointment: ";
+            this.lblAppointment.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.White;
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(458, 48);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(198, 35);
+            this.btnInsert.TabIndex = 12;
+            this.btnInsert.Text = "Insert Contact";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.White;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(458, 89);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(198, 35);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update Contact";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.White;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(458, 130);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(198, 35);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete Contact";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.First,
+            this.Last,
+            this.Email,
+            this.PhoneNumber,
+            this.Category,
+            this.Appointment});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(644, 229);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(169, 9);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(283, 20);
+            this.txtFirst.TabIndex = 16;
+            // 
+            // txtLast
+            // 
+            this.txtLast.Location = new System.Drawing.Point(169, 32);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(283, 20);
+            this.txtLast.TabIndex = 17;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(169, 55);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(283, 20);
+            this.txtEmail.TabIndex = 18;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(169, 78);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(283, 20);
+            this.txtPhone.TabIndex = 19;
+            // 
+            // comboCategory
+            // 
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.Items.AddRange(new object[] {
+            "Home",
+            "Mobile",
+            "Work",
+            "Misc"});
+            this.comboCategory.Location = new System.Drawing.Point(169, 101);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(283, 21);
+            this.comboCategory.TabIndex = 20;
+            // 
+            // txtAppointment
+            // 
+            this.txtAppointment.Location = new System.Drawing.Point(169, 124);
+            this.txtAppointment.Name = "txtAppointment";
+            this.txtAppointment.Size = new System.Drawing.Size(283, 20);
+            this.txtAppointment.TabIndex = 21;
+            // 
+            // First
+            // 
+            this.First.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.First.DataPropertyName = "FirstName";
+            this.First.HeaderText = "First Name";
+            this.First.Name = "First";
+            // 
+            // Last
+            // 
+            this.Last.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Last.DataPropertyName = "LastName";
+            this.Last.HeaderText = "Last Name";
+            this.Last.Name = "Last";
+            // 
+            // Email
+            // 
+            this.Email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Email.DataPropertyName = "Email";
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PhoneNumber.DataPropertyName = "Phone";
+            this.PhoneNumber.HeaderText = "PhoneNumber";
+            this.PhoneNumber.Name = "PhoneNumber";
+            // 
+            // Category
+            // 
+            this.Category.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Category.DataPropertyName = "Category";
+            this.Category.HeaderText = "Category";
+            this.Category.Name = "Category";
+            // 
+            // Appointment
+            // 
+            this.Appointment.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Appointment.DataPropertyName = "Appointment";
+            this.Appointment.HeaderText = "Appointment";
+            this.Appointment.Name = "Appointment";
+            // 
+            // btnNew
+            // 
+            this.btnNew.BackColor = System.Drawing.Color.White;
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(458, 9);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(198, 33);
+            this.btnNew.TabIndex = 22;
+            this.btnNew.Text = "New Contact";
+            this.btnNew.UseVisualStyleBackColor = false;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // frmContactList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(669, 412);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtAppointment);
+            this.Controls.Add(this.comboCategory);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.lblAppointment);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblLast);
+            this.Controls.Add(this.lblFirst);
+            this.Name = "frmContactList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contact List";
+            this.Load += new System.EventHandler(this.frmContactList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.Label lblLast;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblAppointment;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;        
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.ComboBox comboCategory;
+        private System.Windows.Forms.TextBox txtAppointment;
+        private System.Windows.Forms.DataGridViewTextBoxColumn First;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Last;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PhoneNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Category;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Appointment;
+        private System.Windows.Forms.Button btnNew;
+    }
+}
+
---
+++ .NET/Contact_List/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contacts
+{
+    public partial class frmContactList : Form
+    {
+        public frmContactList()
+        {
+            InitializeComponent();
+            SetComboBoxDefault();            
+        }
+
+        protected void SetComboBoxDefault()
+        {
+            comboCategory.SelectedIndex = 0;
+        }
+
+        protected void ClearFields()
+        {
+            txtFirst.Text = string.Empty;
+            txtLast.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            SetComboBoxDefault();
+            txtAppointment.Text = string.Empty;
+        }
+
+        private void frmContactList_Load(object sender, EventArgs e)
+        {
+            BindDataGridView();
+        }
+
+        private void BindDataGridView()
+        {
+            CONTACTS_LISTEntities con = new CONTACTS_LISTEntities();
+            var list = con.CONTACTS_LIST.Select(grid => new
+            {
+                grid.FIRSTNAME,
+                grid.LASTNAME,
+                grid.EMAIL,
+                grid.PHONE,
+                grid.CATEGORY,
+                grid.APPOINTMENT
+            }).ToList();
+
+            dataGridView1.DataSource = list;            
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CONTACTS_LISTEntities context = new CONTACTS_LISTEntities();
+
+                CONTACTS_LIST directory = new CONTACTS_LIST();
+
+                directory.FIRSTNAME = txtFirst.Text;
+                directory.LASTNAME = txtLast.Text;
+                directory.EMAIL = txtEmail.Text;
+                directory.PHONE = txtPhone.Text;
+                directory.CATEGORY = comboCategory.SelectedItem.ToString();
+                directory.APPOINTMENT = txtAppointment.Text;
+
+                context.CONTACTS_LIST.Add(directory);
+
+                context.SaveChanges();
+
+                BindDataGridView();
+
+                ClearFields();
+            }
+            catch (InvalidOperationException)
+            {
+
+                MessageBox.Show("Error entering data into the database.");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            CONTACTS_LISTEntities context = new CONTACTS_LISTEntities();
+            DataGridViewRow row = dataGridView1.SelectedRows[0];
+            string temp = row.Cells["First"].Value.ToString();
+            var val = context.CONTACTS_LIST.Where(t => t.FIRSTNAME.Equals(temp)).FirstOrDefault();
+            val.FIRSTNAME = txtFirst.Text;
+            val.LASTNAME = txtLast.Text;
+            val.EMAIL = txtEmail.Text;
+            val.PHONE = txtPhone.Text;
+            val.CATEGORY = comboCategory.SelectedItem.ToString();
+            val.APPOINTMENT = txtAppointment.Text;
+            context.SaveChanges();
+            BindDataGridView();
+            ClearFields();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            CONTACTS_LISTEntities context = new CONTACTS_LISTEntities();
+            DataGridViewRow row = dataGridView1.SelectedRows[0];
+            string temp = row.Cells["First"].Value.ToString();
+            var val = context.CONTACTS_LIST.Where(t => t.FIRSTNAME.Equals(temp)).FirstOrDefault();
+            context.CONTACTS_LIST.Remove(val);
+            context.SaveChanges();
+            BindDataGridView();
+            ClearFields();
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.SelectedRows[0];
+            string val = row.Cells["First"].Value.ToString();
+            CONTACTS_LISTEntities context = new CONTACTS_LISTEntities();
+            var i = context.CONTACTS_LIST.Where(t => t.FIRSTNAME.Equals(val)).FirstOrDefault();
+            txtFirst.Text = i.FIRSTNAME;
+            txtLast.Text = i.LASTNAME;
+            txtEmail.Text = i.EMAIL;
+            txtPhone.Text = i.PHONE;
+            comboCategory.SelectedItem = i.CATEGORY;
+            txtAppointment.Text = i.APPOINTMENT;
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+    }
+}
